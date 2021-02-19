949969b56648047e9380a2dd66e944486eb05520
ArkOfReis <70862343+ArkOfReis@users.noreply.github.com>
2021-02-19 09:18:39 +0100

    PDF Reader Script

---
+++ app.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+import PyPDF2
+from tkinter.filedialog import askopenfile
+
+root = tk.Tk()
+root.title("PDF to Text Converter")
+root.iconbitmap("icon.ico")
+
+canvas = tk.Canvas(root, width=600, height=300)
+canvas.grid(columnspan=3, rowspan=3)
+
+
+#instructions
+instructions = tk.Label(root, text="Select a PDF file on your computer to extract all its text", font="Raleway")
+instructions.grid(columnspan=3, column=0, row=1)
+
+def open_file():
+    browse_text.set("Loading...")
+    file = askopenfile(parent=root, mode='rb', title="Choose a file", filetype=[("PDF file", "*.pdf")])
+    if file:
+        read_pdf = PyPDF2.PdfFileReader(file)
+        page = read_pdf.getPage(0)
+        page_content = page.extractText()
+
+        #text box
+        text_box = tk.Text(root, height=10, width=50, padx=15, pady=15)
+        text_box.insert(1.0, page_content)
+        text_box.tag_configure("center", justify="center")
+        text_box.tag_add("center", 1.0, "end")
+        text_box.grid(column=1, row=3)
+
+        browse_text.set("Browse")
+
+#browse button
+browse_text = tk.StringVar()
+browse_btn = tk.Button(root, textvariable=browse_text, command=lambda:open_file(), font="Raleway", bg="#20bebe", fg="white", height=2, width=15)
+browse_text.set("Browse")
+browse_btn.grid(column=1, row=2)
+
+canvas = tk.Canvas(root, width=600, height=250)
+canvas.grid(columnspan=3)
+
+root.mainloop()
